16eb9aeb3d8bc2909ec51fcfdfd89430d35d4b18
fzdwx <likelovec@gmail.com>
2024-10-28 22:25:05 +0800

    feat: add plugin demo

---
+++ myplugin/src/lib.rs
@@ -0,0 +1,17 @@
+use proxywasm::{Plugin, Session};
+
+struct MyPlugin;
+
+impl Plugin for MyPlugin {
+    fn new() -> Self
+    where
+        Self: Sized,
+    {
+        Self {}
+    }
+
+    fn request_filter(&self, session: Session) -> Result<bool, String> {
+        session.write_response_body(b"Hello, World 123123!", true);
+        Ok(true)
+    }
+}
