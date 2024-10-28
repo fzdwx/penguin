008143a149a3f69f16e056ec3aab3cfba60775fd
fzdwx <likelovec@gmail.com>
2024-10-28 22:10:28 +0800

    chore: demo

--- proxywasm/src/lib.rs
+++ proxywasm/src/lib.rs
@@ -1,14 +1,45 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::sync::OnceLock;
+
+pub trait Plugin: Send + Sync {
+    /// Returns a new instance of the extension.
+    fn new() -> Self
+    where
+        Self: Sized;
+
+    fn request_filter(&self, _session: Session) -> Result<bool, String>;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+static PLUGIN: OnceLock<Box<dyn Plugin>> = OnceLock::new();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn register_plugin(build: fn() -> Box<dyn Plugin>) {
+    let _ = PLUGIN.set(build());
+}
+
+#[macro_export]
+macro_rules! register_plugin {
+    ($extension_type:ty) => {
+        #[export_name = "init-plugin"]
+        pub extern "C" fn __init_extension() {
+            api::register_extension(|| Box::new(<$extension_type as api::Extension>::new()));
+        }
+    };
+}
+
+wit_bindgen::generate!({
+    path: "./wit",
+    world: "proxy-wasm",
+    skip:["init-plugin"]
+});
+
+
+struct PluginImpl;
+export!(PluginImpl);
+
+impl Guest for PluginImpl {
+    fn request_filter(session: Session) -> Result<bool, String> {
+        match PLUGIN.get() {
+            Some(ext) => ext.request_filter(session),
+            None => Err("Extension not loaded".to_string()),
+        }
     }
 }
